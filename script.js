1158e02b0c2d79915554e74969482a0931b131b8
Sakiskid <tylerdrew.home@gmail.com>
2020-08-21 18:15:57 -0500

    layout change, unsplash api and ajax working

---
+++ script.js
@@ -0,0 +1,31 @@
+// OWM Key: 57477b62efa91ed093f455c4b16426d8
+// Unsplash Key: JHO_3swVG5HRFrh4OfuEWVOCSgvzxb9XnNrPwDD4Uso
+
+var unsplashAPIKey = "&client_id=JHO_3swVG5HRFrh4OfuEWVOCSgvzxb9XnNrPwDD4Uso";
+var unsplashQueryParams = "";
+
+function addNewLocation() {
+    
+}
+
+function makeUnsplashQuery(query){
+    var queryParameters = "&query=" + query;
+    var unsplashURL = "https://api.unsplash.com/search/photos?" + queryParameters + unsplashAPIKey;
+
+    $.ajax({
+        url: unsplashURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
+
+}
+    
+newLocationInputEl.addEventListener('keyup', function (e) {
+    if (e.key === 'Enter') {
+        event.preventDefault();
+        addNewLocation();
+    }
+});
+
+makeUnsplashQuery("texas");
---
+++ scriptReferences.js
@@ -0,0 +1,17 @@
+/* This is a script I'm using to store all of my variables and references.
+I'm not sure if this is bad practice, but it's something I 
+wanted to try with this project so I can have one single
+place to change all of my variables. */
+
+// --------------------------------- //
+
+// ANCHOR References
+
+var newLocationInputEl = document.getElementById("newLocationInput");
+
+
+// ANCHOR Variables (ints and such)
+
+
+
+
